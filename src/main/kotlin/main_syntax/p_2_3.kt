44da5178718ede1c481b3797750c4ca27e74f4c1
k8abadzheva <make.me.loco@gmail.com>
2024-07-08 17:23:01 +0300

    steps: 2.3, 2.4

---
+++ src/main/kotlin/main_syntax/p_2_3.kt
@@ -0,0 +1,16 @@
+package org.example.main_syntax
+
+private var name: String? = null
+private val name1: String? = null
+private val name2: String? = "777777"
+private val name3: String? = ""
+
+fun main() {
+    val length = name?.length?.toString()
+    val length2 = name?.length ?: 0
+    println(name?.length)
+
+    //---------------
+    var result = (name1?.length ?: 0) + (name2?.length ?: 0) + (name3?.length ?: 0)
+    println(result)
+}
---
+++ src/main/kotlin/main_syntax/p_2_4.kt
@@ -0,0 +1,16 @@
+package org.example.main_syntax
+
+fun main() {
+    var count = 300
+    val food = if (count > 500) {
+        count -= 500
+        "pizza"
+    } else if (count > 200) {
+        count -= 200
+        "shawarma"
+    } else {
+        count -= 50
+        "ramen"
+    }
+    println("You can buy $food")
+}
