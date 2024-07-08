b62a3afbc5f32a0d471bc2965880e52092314e79
k8abadzheva <make.me.loco@gmail.com>
2024-07-09 01:00:18 +0300

    steps: 2.6, 2.7

---
+++ src/main/kotlin/main_syntax/p_2_6.kt
@@ -0,0 +1,60 @@
+package org.example.main_syntax
+
+fun main() {
+    val nameOfMonth = "March"
+    val season = when (nameOfMonth) {
+        "December", "January", "February" -> {
+            "Winter"
+        }
+        "March", "April", "May" -> {
+            "Spring"
+        }
+        "June", "July", "August" -> {
+            "Summer"
+        }
+        "September", "October", "November" -> {
+            "Autumn"
+        }
+        else -> {
+            "Not found"
+        }
+    }
+    println(season)
+
+    val indexOfMonth = 8
+    val seasonByIndex = when (indexOfMonth) {
+        12, 1, 2 -> {
+            "Winter"
+        }
+        3, 4, 5 -> {
+            "Spring"
+        }
+        6, 7, 8 -> {
+            "Summer"
+        }
+        in 9..11 -> {
+            "Autumn"
+        }
+        else -> {
+            "Not found"
+        }
+    }
+    println(seasonByIndex)
+
+    val temp = 80
+    val state = when {
+        temp < 0 -> "solid"
+        temp < 100 -> "liquid"
+        else -> "gaseous"
+    }
+    println(state)
+
+    val time = 1
+    val weather = false
+    val activity = when {
+        time !in 8..22 -> "sleep"
+        weather -> "walking"
+        else -> "reading"
+    }
+    println(activity)
+}
---
+++ src/main/kotlin/main_syntax/p_2_7.kt
@@ -0,0 +1,14 @@
+package org.example.main_syntax
+
+fun main() {
+    // arrays
+    val array = arrayOf(1, 2, 3, 34, 45, null)
+    val array1 = arrayOfNulls<Int?>(10)
+    array[4] = 88
+    println(array[4])
+
+    // arrayList
+    val listOfNumbers: MutableList<Int> = ArrayList<Int>()
+    listOfNumbers.add(8)
+    println(listOfNumbers[0])
+}
