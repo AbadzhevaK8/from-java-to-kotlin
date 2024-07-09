fefca4b49e4b288e899acdbe73f16aa7295df44b
k8abadzheva <make.me.loco@gmail.com>
2024-07-10 02:37:49 +0300

    steps: 2.10

---
+++ src/main/kotlin/main_syntax/p_2_10.kt
@@ -0,0 +1,35 @@
+package org.example.main_syntax
+
+fun main() {
+    val numbers = mutableListOf(1, 2, 3, 88, 2, 3, 9, 0, 4, 5)
+    println(sort(8, 3, 32, 83, 294, 0, -17, 9, 23))
+    println(sort(numbers))
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun crop(str: String) = str.substring(0, 5.coerceAtMost(str.length))
+
+fun sum(vararg numbers: Int): Int {
+    var result = 0
+    for (num in numbers) {
+        result += num
+    }
+    return result
+}
+
+fun sort(numbers: MutableList<Int>): List<Int> {
+    for (i in 1 until numbers.size) {
+        for (j in numbers.size - 1 downTo i) {
+            if (numbers[j] < numbers[j - 1]) {
+                numbers[j] = numbers[j - 1].also{
+                    numbers[j - 1] = numbers[j]
+                }
+            }
+        }
+    }
+    return numbers
+}
+
+fun sort(numbers: Array<Int>) = sort(numbers.toMutableList())
+fun sort(vararg numbers: Int) = sort(numbers.toMutableList())
---
+++ src/main/kotlin/main_syntax/p_2_9.kt
@@ -0,0 +1,31 @@
+package org.example.main_syntax
+
+fun main() {
+    val array = arrayOfNulls<Int?>(101)
+//    for (i in 0 until array.size) {
+//        array[i] = i
+//    }
+//    for (i in array) {
+//        println(i)
+//    }
+//    for (i in 100 downTo 0 step 2) {
+//        println(i)
+//    }
+//    for (i in 0..100) {
+//        array[i] = i
+//    }
+//    for ((index, i) in array.withIndex()) {
+//        array[index] = i?.times(2)
+//    }
+//    for (i in array) {
+//        println(i)
+//    }
+    //--------------
+    val array1 = arrayOfNulls<Int?>(301)
+    for ((index, i) in (300..600).withIndex()) {
+        array1[index] = i
+    }
+    for (i in array1.size - 1 downTo 0 step 5) {
+        println(array1[i])
+    }
+}
